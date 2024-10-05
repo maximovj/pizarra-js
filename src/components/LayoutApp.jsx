1221865f0613c8936f745a6b09746594cb13ecb4
Víctor J <maximovj@outlook.com>
2024-10-05 15:24:42 -0600

    * Se agrega el componente de LayoutApp

---
+++ src/components/LayoutApp.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./LayoutApp.css";
+
+const LayoutApp = () => {
+    const [isAsideOpen, setIsAsideOpen] = useState(true);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+
+    const toggleAside = () => {
+        setIsTransitioning(true); // Iniciar la animación
+        setIsAsideOpen(!isAsideOpen);
+    };
+
+    const handleAnimationEnd = () => {
+        setIsTransitioning(false); // Terminar la animación
+    };
+
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="bg-blue-500 p-4 text-white">
+                <div className="flex justify-start items-center gap-x-2">
+                    <button
+                        className="bg-white text-blue-500 p-2 rounded"
+                        onClick={toggleAside}
+                    >
+                        🍔
+                    </button>
+                    <h1 className="text-2xl">Pizarra JS</h1>
+                </div>
+            </header>
+
+            <div className="flex flex-1 overflow-hidden">
+                {/* El aside se oculta/ muestra con una animación personalizada */}
+                {isAsideOpen && (<>
+                    <aside
+                        className={`bg-zinc-700 w-44 p-4 relative text-white hidden md:block ${isAsideOpen ? 'slide-in' : 'slide-out'} ${!isAsideOpen && !isTransitioning ? 'hidden' : ''}`}
+                        onAnimationEnd={handleAnimationEnd}
+                    >
+                        <nav>
+                            <ul className="flex flex-col gap-8">
+                                <li>🖊 Lápiz</li>
+                                <li>🔲 Borrador</li>
+                            </ul>
+                        </nav>
+                    </aside>
+                </>)}
+
+                {!isAsideOpen && (<>
+                    <aside
+                        className={` bg-zinc-700 w-20 p-4 relative text-white ${!isAsideOpen ? 'slide-in' : 'slide-out'}`}
+                    >
+                        <nav>
+                            <ul className="flex flex-col items-center gap-8">
+                                <li className="text-center">
+                                    <i className="block">🖊</i>
+                                    <small>Lápiz</small>
+                                </li>
+                                <li className="text-center">
+                                    <i className="block">🔲</i>
+                                    <small>Borrador</small>
+                                </li>
+                            </ul>
+                        </nav>
+                    </aside>
+                </>)}
+
+                <main className="flex-1 p-6 bg-white">
+                    <Outlet />
+                </main>
+            </div>
+
+            <footer className="bg-blue-500 p-4 text-white text-center mt-auto">
+                <p>Dev by: Víctor J.</p>
+            </footer>
+        </div>
+    );
+};
+
+export default LayoutApp;
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
+import LayoutApp from "../components/LayoutApp";
 
 const MainRoutes = () => {
     return (
         <BrowserRouter basename={routes.BaseName}>
             <Routes>
-                <Route path={routes.Root} element={<h1>Página de Home</h1>} />
+                <Route path={routes.Root} element={<LayoutApp />}>
+                    <Route path={routes.Root} element={<h1>Página de Home</h1>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
