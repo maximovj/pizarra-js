dbb1fcaf3eaaebb30cdc078b351bbf5f907645dc
Víctor J <maximovj@outlook.com>
2024-10-08 19:56:17 -0600

    * Se agrega animacion para toolbar capas

--- src/components/Layers.jsx
+++ src/components/Layers.jsx
@@ -4,7 +4,7 @@ import LayersToolBar from "./Layers/LayersToolBar";
 const Layers = ({ handleAnimationEnd, isAsideOpen }) => {
     return (
         <>
-            {(<>
+            {!isAsideOpen && (<>
                 <aside
                     className={`bg-zinc-700 w-44 p-4 relative text-white md:block ${!isAsideOpen ? 'slide-in-layers' : 'slide-out-layers'}`}
                     onAnimationEnd={handleAnimationEnd}
